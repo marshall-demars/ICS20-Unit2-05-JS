b51effb9497ad05191a4659823c14f42928b28a0
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-25 17:16:09 +0000

    js file

--- js/script.js
+++ js/script.js
@@ -17,5 +17,5 @@ function calculate () {
 
    // output
   document.getElementById('take-home-income').innerHTML = 'Your pay will be: $' + takeHomeIncome.toFixed(2)
-  document.getElementById('gouvernment-taxes').innerHTML = 'The gouvernment will take: $' + taxes.toFixed(2)
+  document.getElementById('government-taxes').innerHTML = 'The government will take: $' + taxes.toFixed(2)
 }
