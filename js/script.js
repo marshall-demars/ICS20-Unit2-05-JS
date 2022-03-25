081608be5b23d363909ced9db5cf9d8ec6916940
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-25 17:01:13 +0000

    4 files

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Created by: Marshall
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates amount of money made.
+ */
+function calculate () {
+  // input
+  const hourlyRate = parseInt(document.getElementById('hourly-rate').value)
+  const hoursWorked = parseInt(document.getElementById('hours-worked').value)
+
+  // process
+  const takeHomeIncome = (hourlyRate * hoursWorked) * (1.00 - 0.18)
+  const taxes = (hourlyRate * hoursWorked) *  0.18
+
+   // output
+  document.getElementById('take-home-income').innerHTML = 'Your pay will be: $' + takeHomeIncome.toFixed(2)
+  document.getElementById('gouvernment-taxes').innerHTML = 'The gouvernment will take: $' + taxes.toFixed(2)
+}
